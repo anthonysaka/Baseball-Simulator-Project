fd8ddcc4a59f3850da867935f249e68af206cce5
VictorJRosario <52325342+VictorJRosario@users.noreply.github.com>
2019-07-01 11:33:21 -0400

    Prueba 1

--- Prueba_GitHub/src/backEnd/Test.java
+++ Prueba_GitHub/src/backEnd/Test.java
@@ -5,5 +5,5 @@ public class Test {
 	private String prueba;
 	
 	// Esto es una prueba, luego vamos a configurar el repositorio de nuestro proyecto.
-
+	// Hay que crear el repositorio del proyecto.
 }
