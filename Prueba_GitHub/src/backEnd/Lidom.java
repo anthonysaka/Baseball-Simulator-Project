185bf29de81372f50019638b79da2192ac53bc2d
anthonysaka <48063344+anthonysaka@users.noreply.github.com>
2019-07-08 15:27:32 -0400

    Controller Class

---
+++ Prueba_GitHub/src/backEnd/Lidom.java
@@ -0,0 +1,15 @@
+package backEnd;
+
+
+/*  Final project - Baseball Simulator Stats -
+ * 
+ *  Developers: Anthony Sakamoto
+ *              Arnold
+ *              Victor Rodriguez
+ *
+ *              Controller Class - LIDOM (Liga de Béisbol Profesional de República Dominicana)
+ */
+
+public class Lidom {
+
+}
