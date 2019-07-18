8b6497ad3fac035189eea4a85e5248727926295a
anthonysaka <48063344+anthonysaka@users.noreply.github.com>
2019-07-17 23:41:26 -0400

    El main esta comentado, explica por que.

--- Baseball_Simulator_Stats/src/frontEnd/Home.java
+++ Baseball_Simulator_Stats/src/frontEnd/Home.java
@@ -27,6 +27,8 @@ import java.awt.FlowLayout;
 import java.awt.CardLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Home extends JFrame {
 
@@ -40,17 +42,24 @@ public class Home extends JFrame {
 	private JPanel panelMenuBar;
 	private JButton btnEquipos;
 	private JButton btnJugadores;
-	private JLabel lblLogoLidom;
 	private JPanel panelBgHome;
 	private JLabel lblBgImagen;
-	private JLabel label;
+	private JLabel lblBgHome_Play_Blur;
 	private JButton btnMini;
 	private JButton btnClose;
+	private JPanel panelMenuEquipo;
+	private JPanel panelMenuJugadores;
+	private JButton btnRegistrarTeam;
+	private JButton btnListarTeams;
+	private JButton btnNa;
+	private JButton btnRegistrarPlayer;
+	private JButton btnListarPlayers;
+	private JButton btnNa_1;
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -61,7 +70,7 @@ public class Home extends JFrame {
 				}
 			}
 		});
-	}
+	}                 ESTA VENTANA SE LEVANTARA AUTOMATICAMENTE DESDE LA CLASE SplashScreen*/
 
 	/**
 	 * Create the frame.
@@ -96,14 +105,27 @@ public class Home extends JFrame {
 		panelMenuBar.setBackground(new Color(0,30,72));
 
 		btnEquipos = new JButton("EQUIPOS");
-		btnEquipos.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				AddTeam newTeam = new AddTeam();
-				newTeam.setVisible(true);
-				newTeam.setModal(true);
+		btnEquipos.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				//panelMenuEquipo.setSize(170,218);
+				setColorBlue(btnEquipos);
+				panelMenuEquipo.setVisible(true);
+
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				//	panelMenuEquipo.setSize(0,0);
+				resetColor(btnEquipos);
+				panelMenuEquipo.setVisible(false);
 			}
 		});
-		btnEquipos.setOpaque(false);
+		btnEquipos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			
+			}
+		});
 		btnEquipos.setIconTextGap(10);
 		btnEquipos.setHorizontalTextPosition(SwingConstants.RIGHT);
 		btnEquipos.setForeground(new Color(255, 255, 240));
@@ -114,15 +136,26 @@ public class Home extends JFrame {
 		panelMenuBar.add(btnEquipos);
 
 		btnJugadores = new JButton("JUGADORES");
+		btnJugadores.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				//	panelMenuJugadores.setSize(170,218);
+				setColorBlue(btnJugadores);
+				panelMenuJugadores.setVisible(true);
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				//panelMenuJugadores.setSize(0,0);
+				resetColor(btnJugadores);
+				panelMenuJugadores.setVisible(false);
+			}
+		});
 		btnJugadores.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				AddPlayer newPlayer = new AddPlayer();
-				newPlayer.setVisible(true);
-				newPlayer.setModal(true);
+				
 
 			}
 		});
-		btnJugadores.setOpaque(false);
 		btnJugadores.setIconTextGap(10);
 		btnJugadores.setHorizontalTextPosition(SwingConstants.RIGHT);
 		btnJugadores.setForeground(new Color(255, 255, 240));
@@ -132,23 +165,6 @@ public class Home extends JFrame {
 		btnJugadores.setBounds(511, 0, 170, 50);
 		panelMenuBar.add(btnJugadores);
 
-		lblLogoLidom = new JLabel("");
-		lblLogoLidom.setBorder(null);
-		lblLogoLidom.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		lblLogoLidom.setBounds(12, 0, 58, 50);
-		panelMenuBar.add(lblLogoLidom);
-		/** to adjust image at size of JLabel **/
-		ImageIcon logoLidom = new ImageIcon(Home.class.getResource("/imagenes/preview-lidom (2).png"));
-		Icon lgLidom = new ImageIcon(logoLidom.getImage().getScaledInstance(lblLogoLidom.getWidth(), lblLogoLidom.getHeight(), Image.SCALE_SMOOTH));
-		lblLogoLidom.setIcon(lgLidom);
-
-		lblLogoLidom.setVerticalTextPosition(SwingConstants.BOTTOM);
-		lblLogoLidom.setOpaque(true);
-		lblLogoLidom.setIconTextGap(10);
-		lblLogoLidom.setHorizontalAlignment(SwingConstants.CENTER);
-		lblLogoLidom.setForeground(Color.WHITE);
-		lblLogoLidom.setFont(new Font("Consolas", Font.BOLD, 20));
-		lblLogoLidom.setBackground(Color.WHITE);
 
 		btnMini = new JButton("");
 		btnMini.addActionListener(new ActionListener() {
@@ -194,15 +210,230 @@ public class Home extends JFrame {
 		btnClose.setBounds(1884, 0, 26, 30);
 		panelMenuBar.add(btnClose);
 
+		panelMenuEquipo = new JPanel();
+		panelMenuEquipo.setVisible(false);
+		panelMenuEquipo.setBackground(new Color(0,30,72));
+		panelMenuEquipo.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				//panelMenuEquipo.setSize(170,218);
+				setColorBlue(btnEquipos);
+				panelMenuEquipo.setVisible(true);
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				//panelMenuEquipo.setSize(0,0);
+				resetColor(btnEquipos);
+				panelMenuEquipo.setVisible(false);
+			}
+		});
+		panelMenuEquipo.setBounds(329, 50, 170, 218);
+		panelBackGround.add(panelMenuEquipo);
+		panelMenuEquipo.setLayout(null);
+
+		btnRegistrarTeam = new JButton("Registrar");
+		btnRegistrarTeam.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				AddTeam newTeam = new AddTeam();
+				newTeam.setVisible(true);
+				newTeam.setModal(true);
+			}
+		});
+		btnRegistrarTeam.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnRegistrarTeam.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				setColorOrange(btnRegistrarTeam);
+				setColorBlue(btnEquipos);
+				panelMenuEquipo.setVisible(true);
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				resetColor(btnRegistrarTeam);
+				resetColor(btnEquipos);
+				panelMenuEquipo.setVisible(false);
+			}
+		});
+		btnRegistrarTeam.setIconTextGap(10);
+		btnRegistrarTeam.setHorizontalTextPosition(SwingConstants.RIGHT);
+		btnRegistrarTeam.setForeground(new Color(255, 255, 240));
+		btnRegistrarTeam.setFont(new Font("Consolas", Font.BOLD, 22));
+		btnRegistrarTeam.setBorder(null);
+		btnRegistrarTeam.setBackground(new Color(0, 30, 72));
+		btnRegistrarTeam.setBounds(0, 13, 170, 50);
+		panelMenuEquipo.add(btnRegistrarTeam);
+
+		btnListarTeams = new JButton("Listar");
+		btnListarTeams.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				setColorBlue(btnEquipos);
+				setColorOrange(btnListarTeams);
+				panelMenuEquipo.setVisible(true);
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				resetColor(btnListarTeams);
+				resetColor(btnEquipos);
+				panelMenuEquipo.setVisible(false);
+			}
+		});
+		btnListarTeams.setIconTextGap(10);
+		btnListarTeams.setHorizontalTextPosition(SwingConstants.RIGHT);
+		btnListarTeams.setForeground(new Color(255, 255, 240));
+		btnListarTeams.setFont(new Font("Consolas", Font.BOLD, 22));
+		btnListarTeams.setBorder(null);
+		btnListarTeams.setBackground(new Color(0, 30, 72));
+		btnListarTeams.setBounds(0, 76, 170, 50);
+		panelMenuEquipo.add(btnListarTeams);
+
+		btnNa = new JButton("n/a");
+		btnNa.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				panelMenuEquipo.setVisible(true);
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				panelMenuEquipo.setVisible(false);
+			}
+		});
+		btnNa.setIconTextGap(10);
+		btnNa.setHorizontalTextPosition(SwingConstants.RIGHT);
+		btnNa.setForeground(new Color(255, 255, 240));
+		btnNa.setFont(new Font("Consolas", Font.BOLD, 22));
+		btnNa.setBorder(null);
+		btnNa.setBackground(new Color(0, 30, 72));
+		btnNa.setBounds(0, 139, 170, 50);
+		panelMenuEquipo.add(btnNa);
+
+		panelMenuJugadores = new JPanel();
+		panelMenuJugadores.setVisible(false);
+		panelMenuJugadores.setBackground(new Color(0,30,72));
+		panelMenuJugadores.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				//panelMenuJugadores.setSize(170,218);
+				setColorBlue(btnJugadores);
+				panelMenuJugadores.setVisible(true);
+
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				//panelMenuJugadores.setSize(0,0);
+				resetColor(btnJugadores);
+				panelMenuJugadores.setVisible(false);
+			}
+		});
+		panelMenuJugadores.setBounds(511, 50, 170, 218);
+		panelBackGround.add(panelMenuJugadores);
+		panelMenuJugadores.setLayout(null);
+
+		btnRegistrarPlayer = new JButton("Registrar");
+		btnRegistrarPlayer.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				AddPlayer newPlayer = new AddPlayer();
+				newPlayer.setVisible(true);
+				newPlayer.setModal(true);
+			}
+		});
+		btnRegistrarPlayer.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnRegistrarPlayer.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				setColorBlue(btnJugadores);
+				setColorOrange(btnRegistrarPlayer);
+				panelMenuJugadores.setVisible(true);
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				resetColor(btnRegistrarPlayer);
+				resetColor(btnJugadores);
+				panelMenuJugadores.setVisible(false);
+			}
+		});
+		btnRegistrarPlayer.setIconTextGap(10);
+		btnRegistrarPlayer.setHorizontalTextPosition(SwingConstants.RIGHT);
+		btnRegistrarPlayer.setForeground(new Color(255, 255, 240));
+		btnRegistrarPlayer.setFont(new Font("Consolas", Font.BOLD, 22));
+		btnRegistrarPlayer.setBorder(null);
+		btnRegistrarPlayer.setBackground(new Color(0, 30, 72));
+		btnRegistrarPlayer.setBounds(0, 13, 170, 50);
+		panelMenuJugadores.add(btnRegistrarPlayer);
+
+		btnListarPlayers = new JButton("Listar");
+		btnListarPlayers.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				setColorBlue(btnJugadores);
+				setColorOrange(btnListarPlayers);
+				panelMenuJugadores.setVisible(true);
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				resetColor(btnListarPlayers);
+				resetColor(btnJugadores);
+				panelMenuJugadores.setVisible(false);
+			}
+		});
+		btnListarPlayers.setIconTextGap(10);
+		btnListarPlayers.setHorizontalTextPosition(SwingConstants.RIGHT);
+		btnListarPlayers.setForeground(new Color(255, 255, 240));
+		btnListarPlayers.setFont(new Font("Consolas", Font.BOLD, 22));
+		btnListarPlayers.setBorder(null);
+		btnListarPlayers.setBackground(new Color(0, 30, 72));
+		btnListarPlayers.setBounds(0, 76, 170, 50);
+		panelMenuJugadores.add(btnListarPlayers);
+
+		btnNa_1 = new JButton("n/a");
+		btnNa_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				panelMenuJugadores.setVisible(true);
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				panelMenuJugadores.setVisible(false);
+			}
+		});
+		btnNa_1.setIconTextGap(10);
+		btnNa_1.setHorizontalTextPosition(SwingConstants.RIGHT);
+		btnNa_1.setForeground(new Color(255, 255, 240));
+		btnNa_1.setFont(new Font("Consolas", Font.BOLD, 22));
+		btnNa_1.setBorder(null);
+		btnNa_1.setBackground(new Color(0, 30, 72));
+		btnNa_1.setBounds(0, 139, 170, 50);
+		panelMenuJugadores.add(btnNa_1);
+
 		panelBgHome = new JPanel();
 		panelBgHome.setBounds(0, 63, 1910, 957);
 		panelBackGround.add(panelBgHome);
 		panelBgHome.setLayout(new CardLayout(0, 0));
 
-		label = new JLabel("");
-		panelBgHome.add(label, "name_5748905232900");
-		label.setIcon(new ImageIcon(Home.class.getResource("/imagenes/fondoPlay.png")));
+		lblBgHome_Play_Blur = new JLabel("");
+		lblBgHome_Play_Blur.setVisible(false);
+		panelBgHome.add(lblBgHome_Play_Blur, "name_5748905232900");
+		lblBgHome_Play_Blur.setIcon(new ImageIcon(Home.class.getResource("/imagenes/fondoPlay.png")));
 
 
+	}
+
+
+
+	/****************************************/
+	/* Metodos */
+
+
+	private void setColorBlue (JButton button) {
+		button.setBackground(new Color(21, 101, 192));
+	}
+	
+	private void setColorOrange (JButton button) {
+		button.setBackground(new Color(239, 108, 0));
+	}
+	
+
+	private void resetColor (JButton button) {
+		button.setBackground(new Color(0,30,72));
 	}
 }
